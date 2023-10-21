1b02baf374fed167ccac31c8e372412fe6c99919
devprashantsh <sharmaprash2207@gmail.com>
2023-10-21 20:43:54 +0530

    fetch and upload contacts added with firebase

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Box, Heading, SimpleGrid, Text } from '@chakra-ui/react';
 import ContactCard from './ContactCard';
+import { getContacts } from '../api/contacts';
+const Contacts = ({contacts}) => {
+  
 
-const ContactList = ({ contacts }) => {
   return (
     <Box p="4">
       <Heading size="lg" mb="4">
         Contacts
       </Heading>
       <SimpleGrid columns={2} spacing={4}>
-        {contacts.map((contact) => (
+        {Array.isArray(contacts) ? contacts.map((contact) => (
           <ContactCard key={contact.id} contact={contact} />
-        ))}
+        ))
+        : (
+          <Box>No Contacts found</Box>
+        )
+      
+      }
       </SimpleGrid>
     </Box>
   );
 };
 
-export default ContactList;
+export default Contacts;
